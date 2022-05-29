731c34764606ac939fe1f9326d10df58cda70821
Илья Максименко <ilamaksimenko91@gmail.com>
2022-05-29 22:42:17 +0300

    Percent fix

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,7 +2,7 @@ public class CreditPaymentService {
 
     public double payment(int a, int b) {
 
-        double percent = 0.008325;
+        double percent = 9.99 / (100 * 12);  //годовой процент разделенный на каждый месяц
 
         return a * (percent * Math.pow((1 + percent), (b * 12))) / (Math.pow((1 + percent), (b * 12)) - 1);
 
