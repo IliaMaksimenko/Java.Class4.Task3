87150f719121ee75031f2b6937dd523042f49024
Илья Максименко <ilamaksimenko91@gmail.com>
2022-05-29 20:40:28 +0300

    NoSwitch

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -4,19 +4,8 @@ public class CreditPaymentService {
 
         double percent = 0.008325;
 
-        switch (b) {
-            case 1:
-                return a * (percent * Math.pow((1 + percent), 12)) / (Math.pow((1 + percent), 12) - 1);
-            case 2:
-                return a * (percent * Math.pow((1 + percent), 24)) / (Math.pow((1 + percent), 24) - 1);
-            case 3:
-                return a * (percent * Math.pow((1 + percent), 36)) / (Math.pow((1 + percent), 36) - 1);
-            case 4:
-                return a * (percent * Math.pow((1 + percent), 48)) / (Math.pow((1 + percent), 48) - 1);
-            case 5:
-                return a * (percent * Math.pow((1 + percent), 60)) / (Math.pow((1 + percent), 60) - 1);
-            default:
-                return a * 0;
-        }
+        return a * (percent * Math.pow((1 + percent), (b * 12))) / (Math.pow((1 + percent), (b * 12)) - 1);
+
+
     }
 }
